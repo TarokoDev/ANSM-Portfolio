b8e24e3719aadb8bed792c33267c974c6c1fd165
Austin Nathaniel Miranda <donovan.austin.ad@gmail.com>
2024-04-14 23:26:44 +0800

    Added projects section

--- frontend/src/components/pages/projects/Projects.jsx
+++ frontend/src/components/pages/projects/Projects.jsx
@@ -4,8 +4,72 @@ export default function Projects() {
     return (
         <>
             <div className={styles.container}>
-                <div className="header">Projects</div>
-                <div className="body"></div>
+                <div className={styles.component}>
+                    <div className={styles.project}>
+                        <div className={styles.image}>
+                            <img
+                                src="/yab.png"
+                                alt="YAB"
+                                className={styles.image}
+                            />
+                        </div>
+                        <div className={styles.description}>
+                            <div className={styles.title}>You are broke!</div>
+                            <div className={styles.summary}>
+                                YAB is a full-stack finance/budgeting management
+                                app.
+                            </div>
+                            <div className={styles.stack}>
+                                <div>React</div>
+                                <div>Node.js</div>
+                                <div>Express</div>
+                                <div>MongoDB</div>
+                            </div>
+                            <div className={styles.buttons}>
+                                <a
+                                    href="https://youarebroke.netlify.app"
+                                    className={styles.button}
+                                    target="_blank"
+                                >
+                                    Visit
+                                </a>
+                                <a
+                                    href="https://youarebroke.netlify.app/about"
+                                    className={styles.button}
+                                    target="_blank"
+                                >
+                                    More Info
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <div className={styles.project}>
+                        <div className={styles.image}>
+                            <img
+                                src="/yab.png"
+                                alt="YAB"
+                                className={styles.image}
+                            />
+                        </div>
+                        <div className={styles.description}>
+                            <div className={styles.title}>Kimchinc</div>
+                            <div className={styles.summary}>
+                                Kimchinc is a delivery management app.
+                            </div>
+                            <div className={styles.stack}>
+                                <div>React</div>
+                                <div>Node.js</div>
+                                <div>Express</div>
+                                <div>MongoDB</div>
+                            </div>
+                            <div className={styles.buttons}>
+                                <button>Visit</button>
+                                <button>More Info</button>
+                            </div>
+                        </div>
+                    </div> */}
+                </div>
             </div>
         </>
     );
