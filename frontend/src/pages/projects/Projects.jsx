c76f4154d09c99ba4a06cc8e8cf2d48f2094452e
ansmdev <austin.nathan.miranda@gmail.com>
2025-06-12 16:26:29 +0800

    Removed unused imports and lines

--- frontend/src/pages/projects/Projects.jsx
+++ frontend/src/pages/projects/Projects.jsx
@@ -1,19 +1,12 @@
-import { useState } from "react";
 import Project from "./Project.jsx";
 import styles from "./Projects.module.css";
 import ProjectsData from "./ProjectsData.jsx";
-import Spline from "@splinetool/react-spline";
 
 export default function Projects() {
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.component}>
-                    {/* <Project splineSrc={"https://prod.spline.design/qSZymrbGSnd47ZSY/scene.splinecode"} data={ProjectsData[0]} />
-                    <Project splineSrc={"https://prod.spline.design/qSZymrbGSnd47ZSY/scene.splinecode"} data={ProjectsData[1]} />
-                    <Project splineSrc={"https://prod.spline.design/2UGn-4CiZicge5nO/scene.splinecode"} data={ProjectsData[2]} />
-                    <Project splineSrc={"https://prod.spline.design/PivuAol3Z3WfYQRU/scene.splinecode"} data={ProjectsData[3]} /> */}
-                    {/* <Project splineSrc={"https://prod.spline.design/qSZymrbGSnd47ZSY/scene.splinecode"} data={ProjectsData[3]} /> */}
                     <Project
                         image={"/visualalgoflow.png"}
                         imageOrientation="landscape"
